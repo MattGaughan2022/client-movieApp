251a9e59eea7bb2d49cbe13039983d32eba97a8b
Matt Gaughan <mattgaugs@gmail.com>
2023-06-12 10:51:46 -0400

    integrate movie API with react

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,45 +3,30 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      Title: "Black Panther",
-      Description: "The Black Panther description is under construction...",
-      Imageurl:
-        "https://m.media-amazon.com/images/M/MV5BMTg1MTY2MjYzNV5BMl5BanBnXkFtZTgwMTc4NTMwNDI@._V1_.jpg",
-      ReleaseYear: "2018",
-      Ratings: "7.3",
-      Director: "Ryan Coogler",
-      Genre: "Action",
-    },
-    {
-      id: 2,
-      Title: "Coco",
-      Description:
-        "Despite his family's generations-old ban on music, young Miguel dreams of becoming an accomplished musician like his idol Ernesto de la Cruz. Desperate to prove his talent, Miguel finds himself in the stunning and colorful Land of the Dead. After meeting a charming trickster named Héctor, the two new friends embark on an extraordinary journey to unlock the real story behind Miguel's family history.",
-      Imageurl:
-        "https://lumiere-a.akamaihd.net/v1/images/p_coco_19736_fd5fa537.jpeg",
-      ReleaseYear: "2017",
-      Ratings: "8.4",
-      Director: "Adrian Molina",
-      Genre: "Fantasy",
-    },
-    {
-      id: 3,
-      Title: "Spirited Away",
-      Description:
-        "In this animated feature by noted Japanese director Hayao Miyazaki, 10-year-old Chihiro (Rumi Hiiragi) and her parents (Takashi Naitô, Yasuko Sawaguchi) stumble upon a seemingly abandoned amusement park. After her mother and father are turned into giant pigs, Chihiro meets the mysterious Haku (Miyu Irino), who explains that the park is a resort for supernatural beings who need a break from their time spent in the earthly realm, and that she must work there to free herself and her parents.",
-      Imageurl:
-        "https://m.media-amazon.com/images/M/MV5BMjlmZmI5MDctNDE2YS00YWE0LWE5ZWItZDBhYWQ0NTcxNWRhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
-      ReleaseYear: "2002",
-      Ratings: "8.6",
-      Director: "Hayao Miyazaki",
-      Genre: "Anime",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://node-movie-api-mattg.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc._id,
+            Title: doc.Title,
+            Imageurl: doc.Imageurl,
+            ReleaseYear: doc.ReleaseYear,
+            Description: doc.Description,
+            Ratings: doc.Ratings,
+            Genre: doc.Genre,
+            Director: doc.Director,
+          };
+        });
+        setMovies(moviesFromApi);
+      });
+  }, []);
+
   if (selectedMovie) {
     return (
       <MovieView
